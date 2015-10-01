1a141e50a46219d1dceb57c78384f77fa1f09e77
Justin Tan <justin.tanwk@gmail.com>
2015-10-01 12:57:00 -0700

    Added bowtie indexes placed in Indices folder. ec_riboprofiling.sh changed to reflect new location of index files.

--- ec_riboprofiling_processing.sh
+++ ec_riboprofiling_processing.sh
@@ -22,15 +22,15 @@ do
 	for index in ec_all_btw ec_trna_btw ec_rRNA_ext_btw ec_RNA_other_btw 
 	do
 		echo 'Now mapping against the following index: '$index    
-		bowtie -n 1 -q -p 4 -S ../$index ${fastq%.*}_trimmed.fastq ${fastq%.*}_${index}_aligned.sam
+		bowtie -n 1 -q -p 4 -S ./Indices/$index ${fastq%.*}_trimmed.fastq ${fastq%.*}_${index}_aligned.sam
 	done
 	
 	echo 'Now mapping against all RNA species index'
-	bowtie -n 1 -q -p 4 ../ec_RNA_all_btw ${fastq%.*}_trimmed.fastq --un ${fastq%.*}_${index}_unaligned.fastq > reads.fastq
+	bowtie -n 1 -q -p 4 ./Indices/ec_RNA_all_btw ${fastq%.*}_trimmed.fastq --un ${fastq%.*}_${index}_unaligned.fastq > reads.fastq
 	rm reads.fastq
 	
 	echo 'Now mapping unaligned reads to reference genome'
-	bowtie -n 1 -q -p 4 -S ../ec_all_btw ${fastq%.*}_${index}_unaligned.fastq ${fastq%.*}_non_RNA_aligned.sam
+	bowtie -n 1 -q -p 4 -S ./Indices/ec_all_btw ${fastq%.*}_${index}_unaligned.fastq ${fastq%.*}_non_RNA_aligned.sam
 
     python ribo_profiling_read_len_dist.py ${fastq%.*}*trna*.sam ${fastq%.*}*rRNA*.sam ${fastq%.*}*RNA_other*.sam ${fastq%.*}*non_RNA*.sam ${fastq%.*}*all*.sam
     rm ${fastq%.*}*non_RNA*.gff
